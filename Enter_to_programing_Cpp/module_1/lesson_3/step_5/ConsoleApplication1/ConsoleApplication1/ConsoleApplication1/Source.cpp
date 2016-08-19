c1427e634f9b66f1240b792aec5996ac4adedff3
847C0N <butterfly.conscious@yandex.ru>
2016-08-20 00:28:33 +0400

    lazy commit

---
+++ Enter_to_programing_Cpp/module_1/lesson_3/step_5/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,16 @@
+//#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int natural_namber;
+	cin >> natural_namber;
+
+	cout << natural_namber % 10;
+
+	_getch();
+	return 0;
+}
---
+++ Enter_to_programing_Cpp/module_1/lesson_3/step_6/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -0,0 +1,16 @@
+
+//#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	int numba;
+	cin >> numba;
+	cout << numba / 10;
+
+	_getch();
+	return 0;
+}
